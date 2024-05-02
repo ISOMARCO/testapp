16383154e248ff88093432643692661204bdf0c9
root <root@vmi1570124.contaboserver.net>
2024-05-02 17:25:36 +0400

    1

---
+++ Modules/FrontApp/Database/Seeders/FrontAppDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\FrontApp\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FrontAppDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
