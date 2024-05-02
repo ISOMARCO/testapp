7b1b617bfb000875df424e4386df92f89ba37fcc
ISOMARCO <inagiyev@icloud.com>
2024-05-02 17:32:51 +0400

    1

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \Modules\FrontApp\App\Providers\FrontAppServiceProvider::class
     ])->toArray(),
 
     /*
