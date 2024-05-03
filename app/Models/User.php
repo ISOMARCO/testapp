3ffc09c00101c2cfcfeac8de2b1636cf84c1525f
ISOMARCO <inagiyev@icloud.com>
2024-05-03 18:16:58 +0400

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\Softdeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Attribute;
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Softdeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     protected string $table = 'users';
 
