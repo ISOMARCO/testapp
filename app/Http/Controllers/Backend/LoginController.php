a643f628ba43a6a3c65943b6af2efbb1de8596a8
ISOMARCO <inagiyev@icloud.com>
2024-05-03 14:53:49 +0400

    1

--- app/Http/Controllers/Backend/LoginController.php
+++ app/Http/Controllers/Backend/LoginController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function index() : View
     {
-        return view('Backend.auth.login');
+        return view('Backend.Auth.login');
     }
 }
