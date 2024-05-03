f3945584f3023288310688ee5ebf0f272dec8c70
ISOMARCO <inagiyev@icloud.com>
2024-05-03 11:06:13 +0400

    1

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.master')
 @section('content')
-    demo
+    <div class="d-flex flex-column flex-column-fluid">
+        <!--begin::Content-->
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <!--begin::Content container-->
+            <div id="kt_app_content_container" class="app-container container-fluid">
+                demo content
+            </div>
+        </div>
+    </div>
 @endsection
