1c4f7684d857d888b7b0654ea49f12b792e2cfa2
ISOMARCO <inagiyev@icloud.com>
2024-05-03 11:47:07 +0400

    1

--- resources/views/sections/auth/aside.blade.php
+++ resources/views/sections/auth/aside.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{asset('frontapp/assets/media/misc/auth-bg.png')}})">
+<div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{asset('assets/media/misc/auth-bg.png')}})">
     <!--begin::Content-->
     <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
         <!--begin::Logo-->
