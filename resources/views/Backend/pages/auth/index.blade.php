7ecc81ce1a741eee3dcc32329c18c4d3b0a54e4b
ISOMARCO <inagiyev@icloud.com>
2024-05-03 16:16:20 +0400

    1

--- resources/views/Backend/pages/auth/index.blade.php
+++ resources/views/Backend/pages/auth/index.blade.php
@@ -43,7 +43,8 @@
                     <!--begin::Submit button-->
                     <div class="d-grid mb-10">
                         <button type="button" id="kt_sign_in_submit" class="btn btn-primary">
-                            <span class="indicator-label">Submit <i class="fas fa-spinner fa-spin"></i></span>
+                            <span class="indicator-label">Submit</span>
+                            <i class="fas fa-spinner fa-spin fa-lg"></i>
                         </button>
                     </div>
                     <!--end::Submit button-->
