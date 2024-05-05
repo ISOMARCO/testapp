ab946344f114b3144470f455a113c3294e8320f8
ISOMARCO <inagiyev@icloud.com>
2024-05-05 12:50:41 +0400

    1

--- resources/views/Backend/layouts/auth.blade.php
+++ resources/views/Backend/layouts/auth.blade.php
@@ -52,7 +52,7 @@
 <script src="{{asset('assets/js/scripts.bundle.js')}}"></script>
 <!--end::Global Javascript Bundle-->
 <!--begin::Custom Javascript(used for this page only)-->
-<script src="{{asset('assets/js/custom/authentication/sign-in/general.js')}}"></script>
+{{--<script src="{{asset('assets/js/custom/authentication/sign-in/general.js')}}"></script>--}}
 <!--end::Custom Javascript-->
 <!--end::Javascript-->
 <script>
