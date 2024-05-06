b6f2f730b5e9b9e9c47fb23ff3f0be5f516769ca
ISOMARCO <inagiyev@icloud.com>
2024-05-06 12:07:24 +0400

    1

--- resources/views/Backend/pages/categories/index.blade.php
+++ resources/views/Backend/pages/categories/index.blade.php
@@ -5,7 +5,54 @@
         <div id="kt_app_content" class="app-content flex-column-fluid">
             <!--begin::Content container-->
             <div id="kt_app_content_container" class="app-container container-fluid">
-                Kateqoriyalar
+                <div class="card card-flush">
+                    <div class="card-body pt-10">
+
+                        <div class="d-flex flex-stack mb-5">
+                            <div class="d-flex align-items-center position-relative my-1">
+                                <i class="ki-duotone ki-magnifier fs-1 position-absolute ms-6"><span class="path1"></span><span
+                                        class="path2"></span></i>
+                                <input type="text" data-kt-docs-table-filter="search" class="form-control  w-250px ps-15"
+                                       placeholder="Search" />
+                            </div>
+                            <div class="d-flex justify-content-end" data-kt-docs-table-toolbar="base">
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#kt_modal_new_target">
+                                    <i class="ki-duotone ki-plus fs-2"></i>
+                                    New Category
+                                </button>
+                            </div>
+                            <div class="d-flex justify-content-end align-items-center d-none"
+                                 data-kt-docs-table-toolbar="selected">
+                                <div class="fw-bold me-5">
+                                    <span class="me-2" data-kt-docs-table-select="selected_count"></span> Selected
+                                </div>
+
+                                <button type="button" class="btn btn-danger" data-bs-toggle="tooltip" title="Coming Soon">
+                                    Selection Action
+                                </button>
+                            </div>
+                        </div>
+                        <table style="border-radius: 10px; overflow: hidden" id="kt_datatable_example_1"
+                               class="table table-striped table-row-bordered align-middle fs-6 gy-5">
+                            <thead>
+                            <tr class="text-start text-white bg-primary fw-bold fs-7 text-uppercase gs-0">
+                                <th class="px-3 w-40px">Id
+                                </th>
+                                <th class="min-w-125px">{{ __('frontapp::general.pages.users.list.user') }}</th>
+                                <th>Name</th>
+                                <th>Parent</th>
+                                <th class="text-end pe-15 ">{{ __('frontapp::general.shared.actions') }}</th>
+                            </tr>
+                            </thead>
+                            <tbody class="text-gray-600 fw-semibold">
+                            </tbody>
+                        </table>
+                        <!--end::Datatable-->
+                        <!--end::Table-->
+                    </div>
+                    <!--end::Card body-->
+                </div>
             </div>
         </div>
     </div>
