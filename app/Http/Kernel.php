a09df9e3008c25cd28f3fec2269b84507a46895f
ISOMARCO <inagiyev@icloud.com>
2024-05-06 11:16:46 +0400

    1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,9 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            Authenticate::class,
+            RedirectIfAuthenticated::class
         ]
     ];
 
