0cfea6f33a9c509fe1c34f0b621bd4fb7962b77a
ISOMARCO <inagiyev@icloud.com>
2024-05-06 16:50:51 +0400

    1

--- app/Http/Requests/Backend/Categories/CategoriesRequest.php
+++ app/Http/Requests/Backend/Categories/CategoriesRequest.php
@@ -24,7 +24,7 @@ class CategoriesRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|min:3|max:100',
+            'name' => 'required|string|min:3|max:100|unique:categories,name',
             'id' => 'required|integer'
         ];
     }
--- database/migrations/2024_05_06_081724_categories.php
+++ database/migrations/2024_05_06_081724_categories.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable();
             $table->timestamp('deleted_at')->nullable();
