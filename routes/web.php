f66bb2f3143bb24fc7cec5a6e5b929ad94f05261
ISOMARCO <inagiyev@icloud.com>
2024-05-06 11:23:00 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/admin', [App\Http\Controllers\Backend\Home\HomeController::class, 'index'])->name('Backend.home');
+Route::redirect('/admin/home', '/admin');
