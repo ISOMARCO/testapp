bfc86aa582357ae215732d49d761a436fced3a07
ISOMARCO <inagiyev@icloud.com>
2024-05-06 15:57:09 +0400

    1

--- routes/Backend/Categories/web.php
+++ routes/Backend/Categories/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Categories\CategoriesController;
 
 Route::get('/categories', [CategoriesController::class, 'index'])->name('index');
-Route::post('/categories/update', [CategoriesController::class, 'updateRequest'])->name('update');
+Route::post('/categories/update/{category}', [CategoriesController::class, 'updateRequest'])->name('update');
