8c6a12ac69824e7c01a61a0996e18ebbdbc360b0
ISOMARCO <inagiyev@icloud.com>
2024-05-06 10:05:08 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//Route::get('/admin', []);
+Route::get('/admin', [App\Http\Controllers\Backend\Home\HomeController::class, 'index'])->name('Backend.home');
