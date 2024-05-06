6009296d7735487478194a6ee2d26ccb7f3b86e5
ISOMARCO <inagiyev@icloud.com>
2024-05-06 11:57:17 +0400

    1

--- app/Http/Controllers/Backend/Categories/CategoriesController.php
+++ app/Http/Controllers/Backend/Categories/CategoriesController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('Backend.pages.categories.index');
+    }
 }
