02cf777ad9e863fa21b6c410e622bc216edec94f
ISOMARCO <inagiyev@icloud.com>
2024-05-07 11:18:25 +0400

    1

--- resources/views/Backend/pages/customers/index.blade.php
+++ resources/views/Backend/pages/customers/index.blade.php
@@ -63,17 +63,17 @@
                                     </a>
                                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-150px py-4" data-kt-menu="true">
                                         <div class="menu-item px-3">
-                                            <a class="menu-link px-3" data-kt-docs-table-filter="see_row">
+                                            <a class="menu-link px-3" data-kt-docs-table-filter="see_row" data-bs-target="#see_customer" id="see_row">
                                                 <i class="fas fa-eye menu-bullet"></i> Ətraflı bax
                                             </a>
                                         </div>
                                         <div class="menu-item px-3">
-                                            <a data-bs-toggle="modal" data-bs-target="#edit_category" data-id="{{$customer->id}}" data-kt-docs-table-filter="edit_row" class="menu-link px-3" id="edit_row">
+                                            <a data-bs-toggle="modal" data-bs-target="#edit_customer" data-id="{{$customer->id}}" data-kt-docs-table-filter="edit_row" class="menu-link px-3" id="edit_row">
                                                 <i class="fas fa-edit menu-bullet"></i> Dəyiş
                                             </a>
                                         </div>
                                         <div class="menu-item px-3">
-                                            <a class="menu-link px-3" data-kt-docs-table-filter="delete_row">
+                                            <a class="menu-link px-3" data-kt-docs-table-filter="delete_customer" id="delete_row">
                                                 <i class="fas fa-trash-alt menu-bullet"></i> Sil
                                             </a>
                                         </div>
