3aabf3892162226906064034f410219447863805
root <root@vmi1570124.contaboserver.net>
2024-05-07 10:55:03 +0400

    1

---
+++ app/Http/Controllers/Backend/Customers/CustomersController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Customers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CustomersController extends Controller
+{
+    //
+}
