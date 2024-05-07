02b8a2e9232ec6632ad97f90c33834c9938df307
ISOMARCO <inagiyev@icloud.com>
2024-05-07 10:43:16 +0400

    1

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Attribute;
+
 class Category extends Model
 {
     use HasFactory, SoftDeletes;
