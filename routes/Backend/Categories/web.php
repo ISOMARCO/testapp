6e27b3726e1b9b32933755dde14fa48b49533927
ISOMARCO <inagiyev@icloud.com>
2024-05-07 10:05:55 +0400

    1

--- routes/Backend/Categories/web.php
+++ routes/Backend/Categories/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Categories\CategoriesController;
 
-Route::get('/categories', [CategoriesController::class, 'index'])->name('index');
-Route::post('/categories/update', [CategoriesController::class, 'updateRequest'])->name('update');
-Route::post('/categories/create', [CategoriesController::class, 'createRequest'])->name('create');
+Route::get('/categories', [CategoriesController::class, 'index'])->name('index')->middleware('auth');
+Route::post('/categories/update', [CategoriesController::class, 'updateRequest'])->name('update')->middleware('auth');
+Route::post('/categories/create', [CategoriesController::class, 'createRequest'])->name('create')->middleware('auth');
