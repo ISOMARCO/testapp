316eefd901d801beffd7ad42001435a12f90f26f
ISOMARCO <inagiyev@icloud.com>
2024-05-07 10:58:27 +0400

    1

--- routes/Backend/Customers/web.php
+++ routes/Backend/Customers/web.php
@@ -1,3 +1,5 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\Customers\CustomersController;
 
+Route::get('/customers', [CustomersController::class, 'index'])->name('index')->middleware('auth');
