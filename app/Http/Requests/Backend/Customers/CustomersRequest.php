bf6407611dba4ff1ca22e2a338398ee2e2525ae5
ISOMARCO <inagiyev@icloud.com>
2024-05-07 14:38:25 +0400

    1

--- app/Http/Requests/Backend/Customers/CustomersRequest.php
+++ app/Http/Requests/Backend/Customers/CustomersRequest.php
@@ -24,13 +24,13 @@ class CustomersRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string',
-            'surname' => 'required|string',
-            'email' => 'required|email',
+            'name' => 'required|string|min:3|max:70',
+            'surname' => 'required|string|min:3|max:70',
+            'email' => 'required|email|unique:users,email|max:150',
             'country' => 'required|string',
             'category' => 'required|string',
-            'password' => 'required|string',
-            'password_confirmation' => 'required|string|same:password'
+            'password' => 'required|string|min:5|max:100',
+            'password_confirmation' => 'required|string|same:password|min:5|max:100'
         ];
     }
 
@@ -48,7 +48,17 @@ class CustomersRequest extends FormRequest
             'category.required' => 'Kateqoriya daxil edilməlidir',
             'password.required' => 'Şifrə daxil edilməlidir',
             'password_confirmation.required' => 'Şifrə təsdiqi daxil edilməlidir',
-            'password_confirmation.same' => 'Şifrə təsdiqi şifrə ilə uyğun gəlmir'
+            'password_confirmation.same' => 'Şifrə təsdiqi şifrə ilə uyğun gəlmir',
+            'password.min' => 'Şifrə minimum 5 simvoldan ibarət olmalıdır',
+            'password.max' => 'Şifrə maksimum 100 simvoldan ibarət olmalıdır',
+            'password_confirmation.min' => 'Şifrə təsdiqi minimum 5 simvoldan ibarət olmalıdır',
+            'password_confirmation.max' => 'Şifrə təsdiqi maksimum 100 simvoldan ibarət olmalıdır',
+            'email.unique' => 'Bu email artıq istifadə olunub',
+            'name.min' => 'Ad minimum 3 simvoldan ibarət olmalıdır',
+            'name.max' => 'Ad maksimum 70 simvoldan ibarət olmalıdır',
+            'surname.min' => 'Soyad minimum 3 simvoldan ibarət olmalıdır',
+            'surname.max' => 'Soyad maksimum 70 simvoldan ibarət olmalıdır',
+            'email.max' => 'Email maksimum 150 simvoldan ibarət olmalıdır'
         ];
     }
 
